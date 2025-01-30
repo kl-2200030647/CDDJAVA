df3c0bd9bfa552c58729dd30b33a61a8ab288a9c
kl-2200030647 <153382400+kl-2200030647@users.noreply.github.com>
2025-01-30 11:43:04 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+        int sum = num1 + num2; // Adding both numbers
+
+        System.out.println("Sum: " + sum);
+    }
+}
